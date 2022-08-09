1b1e7360751b6f2b65562b679e95d7dbc32ed7ae
lucaslmeireles <lluciomeireles@gmail.com>
2022-08-09 21:42:13 +0000

    adicionando icons, ainda faltam alguns

--- screens/HomePageNew.js
+++ screens/HomePageNew.js
@@ -20,25 +20,18 @@ export const HomePageNew = () => {
             className='w-7 h-7 mx-3'
             />
         </View>
-        <View className='flex-row justify-around'>
-            <Text className='text-xl pt-3 mx-3 mt-4'>
+        <View className='flex-row justify-start ml-1'>
+            <Text className='text-xl pt-3 mx-3  mt-4'>
                 Popular
                 <View className='rounded-full bg-slate-800 w-2 h-2 r-1 align-middle'></View>
             </Text>
-            
-            <Text className='text-xl pt-3 mx-3 mt-4 text-gray-700'>
-                Recent
-            </Text>
-            <Text className='text-xl pt-3 mx-3 mt-4 text-gray-700'>
-                Recommend
-            </Text>
         </View>
-        <View className='mt-4 justify-center items-center'>
+        <View className='mt-3'>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 <TouchableOpacity className='my-3 mx-5' onPress={() => navigation.navigate('PostDetail')}>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2020/11/04/19/22/old-windmill-5713337_960_720.jpg'}}
-                    className='w-72 h-72 rounded-3xl '
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-white font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-white font-medium'>Impactos ambientais do turismo</Text>
@@ -47,10 +40,7 @@ export const HomePageNew = () => {
                 <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2016/09/21/04/46/barley-field-1684052__340.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
-                    />
-                    <LinearGradient
-                        colors={['rgba(0,0,0,0.8)', 'transparent']}
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-black font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-black font-medium'>Impactos ambientais do turismo</Text>
@@ -59,14 +49,20 @@ export const HomePageNew = () => {
                 <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2015/12/01/20/28/forest-1072828_960_720.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-white font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-white font-medium'>Impactos ambientais do turismo</Text>
 
                 </TouchableOpacity>
             </ScrollView>
-            <View></View>
+
+            <View className='justify-start flex-row items-center'>
+                <Text className='text-xl pt-3 mx-3 mt-3 mb-2 text-gray-900'>
+                    Recommend
+                    <View className='rounded-full bg-slate-800 w-2 h-2  align-middle'></View>
+                </Text>
+            </View>
 
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 {/* #TODO Escurecer imagens, tirar o roxo do fim da scroll, 
@@ -74,7 +70,7 @@ export const HomePageNew = () => {
             <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2017/07/29/13/17/plant-2551467__340.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-white font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-white font-medium'>Impactos ambientais do turismo</Text>
@@ -83,7 +79,7 @@ export const HomePageNew = () => {
                 <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2021/08/26/15/18/mountain-6576362__340.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-white font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-white font-medium'>Impactos ambientais do turismo</Text>
@@ -92,7 +88,7 @@ export const HomePageNew = () => {
                                 <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2016/01/19/15/48/luggage-1149289__340.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-black font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-black font-medium'>Impactos ambientais do turismo</Text>
@@ -101,7 +97,7 @@ export const HomePageNew = () => {
                 <TouchableOpacity className='my-3 mx-5'>
                     <Image 
                     source={{uri:'https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581__340.jpg'}}
-                    className='w-72 h-72 rounded-3xl'
+                    className='w-64 h-64 rounded-3xl'
                     />
                     <Text className='absolute top-4 mx-4 text-base text-white font-light'>Ecologia</Text>
                     <Text className='absolute top-8 mx-4 my-3 text-xl text-white font-medium'>Impactos ambientais do turismo</Text>
--- screens/PostNew.js
+++ screens/PostNew.js
@@ -3,28 +3,43 @@ import { StyleSheet, View, Text, Image, Pressable, SafeAreaView, ScrollView, Tou
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from 'expo-linear-gradient'
+import Feather from "@expo/vector-icons/Feather"; 
 
 export const PostNew = () => {
     const navigation = useNavigation();
-
+    const [saved, setSaved] = React.useState(false)
     return (
     <>
     <StatusBar backgroundColor="black"></StatusBar>
-    <View style={{marginTop:36,}}>
+    <View style={{marginTop:36}}>
         <ScrollView >
             {/* Top Header */}
-            <View className='mx-4 my-2'>
+            <View className='mx-5 my-2'>
+                <View className='justify-between flex-row pt-3'>
+                    <Feather name='arrow-left' size={29}/>
+                    <Feather name='share' size={24}/>
+                </View>
             <View className='pb-5 mt-6'>
                 <Text className='text-2xl font-medium'>Ecologia a alma de um educador de hora</Text>
             </View>
-            <View className='justify-start flex-row items-center mt-3'>
-                <Image
-                source={{uri:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0='}}
-                className='rounded-full w-12 h-12 mr-2'
-                />
-                <View>
-                    <Text className='font-normal text-base text-gray-900'>Alissa</Text>
-                    <Text className='font-light text-sm text-gray-500'>3 days ago</Text>
+            <View className='justify-between flex-row items-center mt-3 mb-3'>
+                <View className='justify-start flex-row'>
+                    <Image
+                    source={{uri:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0='}}
+                    className='rounded-full w-12 h-12 mr-2'
+                    />
+                    <View>
+                        <Text className='font-normal text-base text-gray-900'>Alissa</Text>
+                        <Text className='font-light text-sm text-gray-500'>3 days ago</Text>
+                    </View>
+                </View>
+                <View className='justify-start flex-row items-center'>
+                    <Pressable className='mx-2' onPress={() => setSaved(!saved)}>
+                        <Feather name={saved ? 'heart' : 'bookmark'} size={28} />
+                    </Pressable>
+                    <Pressable class='mx-2' >
+                        <Feather name='message-square' size={28}/>
+                    </Pressable>
                 </View>
             </View>
             {/* Text area */}
