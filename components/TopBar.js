60203faf98c5156619011cee0104fda1fcf536d6
lucaslmeireles <lluciomeireles@gmail.com>
2022-08-10 21:16:10 +0000

    todo profilepage

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,14 +1,19 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image,Pressable } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 export default function TopBar() {
+    const navigation = useNavigation();
+
   return (
     <View className='mt-2 ml-1 flex-row justify-between items-center'>
         <View className='justify-start flex-row items-center mt-3 mx-2'>
-            <Image
-            source={{uri:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0='}}
-            className='w-8 h-8 mx-1 rounded-full'
-        />
+            <Pressable onPress={() => navigation.navigate('ProfielPage')}>
+                <Image
+                source={{uri:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0='}}
+                className='w-8 h-8 mx-1 rounded-full'
+                />
+            </Pressable>
         <Text className=' text-2xl mx-1'>
             Explore
         </Text>
